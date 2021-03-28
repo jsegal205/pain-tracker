6e3eb8f7873d49355b33f1f0d6438d35900fb254
Jim Segal <hello@jimsegal.com>
2021-03-28 16:03:47 -0500

    read categories and symptoms from google sheets

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const painCategories = require("./pain-categories.json");
 
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
@@ -18,11 +17,35 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/pain-items", function (req, res) {
-  res.json(painCategories);
+app.get("/pain-items", async (req, res) => {
+  try {
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    });
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[1];
+
+    const rows = await sheet.getRows();
+
+    const data = [];
+    rows.map((row) => {
+      const symptoms = !!row.symptoms ? row.symptoms.split(",") : [];
+
+      return data.push({
+        name: row.name,
+        symptoms: symptoms.map((s) => s.trim()),
+      });
+    });
+
+    res.json(data);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-app.post("/pain-items", (req, res) => {
+app.post("/pain-items", async (req, res) => {
   const { name, selectedSymptoms, optionalNotes } = req.body;
 
   const tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
@@ -30,30 +53,26 @@ app.post("/pain-items", (req, res) => {
     .toISOString()
     .slice(0, -1);
 
-  (async () => {
-    try {
-      // Initialize the sheet - doc ID is the long id in the sheets URL
-      const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
-      await doc.useServiceAccountAuth({
-        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      });
+  try {
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    });
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
 
-      await doc.loadInfo(); // loads document properties and worksheets
+    await sheet.addRow({
+      name,
+      symptoms: selectedSymptoms.join(", "),
+      notes: !!optionalNotes ? optionalNotes.replace(/[\r\n]+/g, ". ") : "",
+      time: localISOTime,
+    });
 
-      const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-      await sheet.addRow({
-        name,
-        symptoms: selectedSymptoms.join(", "),
-        notes: !!optionalNotes ? optionalNotes.replace(/[\r\n]+/g, ". ") : "",
-        time: localISOTime,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  })();
-
-  res.status(200).send();
+    res.status(200).send();
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 // The "catchall" handler: for any request that doesn't
